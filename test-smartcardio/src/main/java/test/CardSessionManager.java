2061e78336a2d88c5c69fab95a545970e9dd46b4
Krish Shah <shahkrish2016@gmail.com>
2024-08-21 23:28:42 -0400

    cleaned up constructor, setup `CardException` everywhere

--- test-smartcardio/src/main/java/test/CardSessionManager.java
+++ test-smartcardio/src/main/java/test/CardSessionManager.java
@@ -3,6 +3,7 @@ package test;
 import javax.smartcardio.*;
 import java.util.List;
 import java.lang.UnsupportedOperationException;
+import java.lang.Exception;
 
 // consider renaming to 'ScannerSessionManager'
 // make custom exception class? (CardException on its own is not always accurate)
@@ -11,35 +12,16 @@ public class CardSessionManager {
     private CardTerminal terminal;
     private CardChannel channel;
 
-    public CardSessionManager() throws Exception { 
-        terminal = null;
-        try {
-            terminal = this.getFirstTerminal();
-        } catch (CardException e) {
-            System.err.println("CardException: " + e.getMessage());
-        } finally {
-            if (terminal == null) {
-                throw new Exception("No card terminals found.");
-            }
-        }
-    
-        channel = null;
-        try {
-            channel = this.identifyCard(terminal);
-        } catch (CardException e) {
-            System.err.println("CardException: " + e.getMessage());
-        } finally {
-            if (channel == null) {
-                throw new Exception("No card found in terminal.");
-            }
-        }
+    public CardSessionManager() throws CardException { 
+        terminal = this.getFirstTerminal();
+        channel = this.identifyCard(terminal);
     }
 
     /**
      * prints all connected terminals and returns an unmodifable list
      * containing each connected terminal object
      */
-    private List<CardTerminal> getLocalTerminals() throws Exception {
+    private List<CardTerminal> getLocalTerminals() throws CardException {
         TerminalFactory factory = TerminalFactory.getDefault();
         List<CardTerminal> terminals = factory.terminals().list();
         // printing terminal statuses
@@ -50,8 +32,9 @@ public class CardSessionManager {
     /**
      * returns the first terminal found
      */
-    private CardTerminal getFirstTerminal() throws Exception {
+    private CardTerminal getFirstTerminal() throws CardException {
         List<CardTerminal> terminals = this.getLocalTerminals();
+
         if (terminals.isEmpty()) {
             throw new CardException("No card terminals found.");
         }
@@ -66,7 +49,7 @@ public class CardSessionManager {
      * then, establishes a connection to a card if one exists and prints all of its
      * data
      */
-    private CardChannel identifyCard(CardTerminal terminal) throws Exception {
+    private CardChannel identifyCard(CardTerminal terminal) throws CardException {
         if (!terminal.isCardPresent()) {
             throw new CardException("No card found in terminal: " + terminal.getName() + ".");
         }
@@ -76,6 +59,7 @@ public class CardSessionManager {
         // T=0 is character oriented protocol, allows for apdus
         Card localCard = terminal.connect("*");
         System.out.println("Local card protocol: " + localCard.getProtocol());
+
         // card found: return channel to facilitate apdus
         return localCard.getBasicChannel();
     }
